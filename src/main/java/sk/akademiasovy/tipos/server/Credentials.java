145f48246c98745344dd91d7826750095fd4206f
borisgalicky <boris.galicky@akademiasovy.sk>
2018-03-08 08:52:12 +0100

    re-created repository with all necessary files

---
+++ src/main/java/sk/akademiasovy/tipos/server/Credentials.java
@@ -0,0 +1,25 @@
+package sk.akademiasovy.tipos.server;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * Created by host on 20.2.2018.
+ */
+public class Credentials {
+    @JsonProperty("username")
+    public String username;
+
+    @JsonProperty("password")
+    public String password;
+
+    @JsonProperty("token")
+    public String token;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
